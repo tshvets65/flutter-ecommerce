e6fecdeebd3bd5da44e53904636aba9d5dd842c5
Tetyana Shvets <tshvets@hotmail.com>
2019-04-20 19:16:35 -0700

    MediaQuery

--- lib/redux/actions.dart
+++ lib/redux/actions.dart
@@ -1,5 +1,6 @@
 import 'dart:convert';
 import 'package:flutter_ecommerce/models/app_state.dart';
+import 'package:flutter_ecommerce/models/product.dart';
 import 'package:flutter_ecommerce/models/user.dart';
 import 'package:redux/redux.dart';
 import 'package:redux_thunk/redux_thunk.dart';
@@ -25,13 +26,18 @@ class GetUserAction {
 ThunkAction<AppState> getProductsAction = (Store<AppState> store) async {
   http.Response response = await http.get('http://localhost:1337/products');
   final List<dynamic> responseData = json.decode(response.body);
-  store.dispatch(GetProductsAction(responseData));
+  List<Product> products = [];
+  responseData.forEach((productData) {
+    final Product product = Product.fromJson(productData);
+    products.add(product);
+  });
+  store.dispatch(GetProductsAction(products));
 };
 
 class GetProductsAction {
-  final List<dynamic> _products;
+  final List<Product> _products;
 
-  List<dynamic> get products => this._products;
+  List<Product> get products => this._products;
 
   GetProductsAction(this._products);
 }
